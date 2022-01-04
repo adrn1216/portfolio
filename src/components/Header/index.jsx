b0a74a2dee711e8aea901e4cf28202b7198f8c11
adrn1216 <louisadrianreyes@yahoo.com>
2022-01-04 21:09:44 +0800

    Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,19 @@
+import { HeaderContainer, Nav } from "./style";
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <div>
+        <p>adrn1216</p>
+        <Nav>
+          <a href="">Intro</a>
+          <a href="">About Me</a>
+          <a href="">Projects</a>
+          <a href="">Contact Me</a>
+        </Nav>
+      </div>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/style.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.div`
+  width: 100%;
+  height: 69px;
+  background-color: var(--bottom-color);
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid var(--edges-color);
+  div {
+    width: 100%;
+    max-width: 1080px;
+    margin: auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    p {
+      font-family: var(--secondary-font);
+      font-size: 24px;
+      margin: 0;
+      color: var(--white-color);
+    }
+  }
+`;
+
+export const Nav = styled.nav`
+  display: flex;
+  a {
+    font-size: 16px;
+    font-weight: 500;
+    margin: 0 12px;
+    text-decoration: none;
+    color: var(--white-color);
+    padding-bottom: 3px;
+    border-bottom: 3px solid transparent;
+    :hover,
+    :visited,
+    .active {
+      border-bottom: 3px solid var(--primary-color);
+    }
+  }
+`;
